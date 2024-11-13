4b69e2a4a8bd88df60a39b02c28320fbf5aade2b
Yahweh's Delight <intricatesyllable@gmail.com>
2024-11-13 05:50:59 +0300

    Pycode style

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -9,7 +9,6 @@ from flask_cors import (CORS, cross_origin)
 from flask import Flask, jsonify, abort, request
 
 
-
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
@@ -21,13 +20,16 @@ def not_found(error) -> str:
     """
     return jsonify({"error": "Not found"}), 404
 
+
 @app.errorhandler(401)
 def unauthorized(error) -> str:
     """ Unauthorized handler
     """
     return jsonify({"error": "Unauthorized"}), 401
 
+
 if __name__ == "__main__":
+
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
     app.run(host=host, port=port)
